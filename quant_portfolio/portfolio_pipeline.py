e7a56010bda9575cb6c5546582ecea87809fcf27
Rishikesh Gokhale <rishihg@gmail.com>
2025-12-18 00:49:05 -0500

    Implement CVaR formulation in portfolio_pipeline
    Added support for CVaR formulation in portfolio optimization.

--- quant_portfolio/portfolio_pipeline.py
+++ quant_portfolio/portfolio_pipeline.py
@@ -76,6 +76,9 @@ class PortfolioPipeline:
             energies = energies_full(problem["means"], problem["cov"], q, N)
         elif formulation == "mad":
             energies = energies_full_mad(problem["returns"], q, N)
+        elif formulation == "cvar":  
+            from .formulations_extended import energies_full_cvar
+            energies = energies_full_cvar(problem["returns"], q, N, alpha=0.05)
             
         else:
             energies = energies_full_mvo_tc(problem["means"], problem["cov"], q, N, problem["tc"], lam_tc)
